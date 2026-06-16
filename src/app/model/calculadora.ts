43a9e4c6887d29decaa4ebbe6e07164a48a5b5b5
Eduardo Vasconcelos <eduardo.melovs@gmail.com>
2026-06-16 14:03:15 -0300

    Forçar um retorno deretornar um tipo

--- src/app/model/calculadora.ts
+++ src/app/model/calculadora.ts
@@ -1,10 +1,18 @@
 export class Calculadora {
 
-    somar(primeiroNumero : number, segundoNumero : number){
+    somar(primeiroNumero : number, segundoNumero : number): number {
         return primeiroNumero + segundoNumero;
     }
 
-    sub(num1 : number, num2 : number){
+    sub(num1 : number, num2 : number): number {
         return num1 - num2;
+    }
+
+    multiplicar(numero1 : number, numero2 : number): number {
+        return numero1 * numero2;
+    }
+
+    dividir(divisor1 : number, divisor2 : number): number {
+        return divisor1 / divisor2;
     }
 }
